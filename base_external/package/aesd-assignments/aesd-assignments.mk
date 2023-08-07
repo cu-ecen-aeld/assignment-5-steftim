1a236ce36598f1f3e0965c4c9c339c7c63056712
Andriiets Bohdan <steftim.an@gmail.com>
2023-08-07 20:51:57 +0300

    Fix some mistakes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'd65912107c8233e93b9fda439d2feb9dff557df4'
+AESD_ASSIGNMENTS_VERSION = '549c0767978d3e82e7aa9c4ca3283b74b146ad14'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
